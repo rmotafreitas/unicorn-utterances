06c2875aee5064a6fa7a6144fa5f2a7ce57b9131
James Fenn <me@jfenn.me>
2022-07-12 11:29:39 -0400

    auto-scroll table of contents when the active heading is out of bounds

--- src/components/table-of-contents/use-heading-intersection-observer.ts
+++ src/components/table-of-contents/use-heading-intersection-observer.ts
@@ -16,18 +16,52 @@ export const useHeadingIntersectionObserver = ({
   const [previousSection, setPreviousSelection] = React.useState("");
 
   React.useEffect(() => {
+    // this assumes the <TableOfContents> is placed in a <BlogPostLayout>, which provides the scrolling container
+    const tocListContainer = tocListRef.current?.parentElement?.parentElement;
+
+    const prefersReducedMotion = window.matchMedia(
+      "(prefers-reduced-motion: reduce)"
+    );
+
     const handleObserver: IntersectionObserverCallback = (entries) => {
       const highlightFirstActive = () => {
         if (!tocListRef.current) return;
-        let firstVisibleLink =
-          tocListRef.current.querySelector(".toc-is-visible");
+        let firstVisibleLink = tocListRef.current.querySelector(
+          ".toc-is-visible"
+        ) as HTMLLIElement;
 
         linkRefs.forEach((linkRef) => {
           linkRef.current!.classList.remove("toc-is-active");
         });
 
-        if (firstVisibleLink) {
+        if (
+          firstVisibleLink &&
+          !firstVisibleLink.classList.contains("toc-is-active")
+        ) {
           firstVisibleLink.classList.add("toc-is-active");
+
+          // if the element is beyond the visible area of the container...
+          if (
+            // the user hasn't requested reduced motion...
+            !prefersReducedMotion.matches &&
+            tocListContainer &&
+            // the link is below the lowest point of the container...
+            (tocListContainer.scrollTop + tocListContainer.offsetHeight <
+              firstVisibleLink.offsetTop ||
+              // the link is above the highest point of the container...
+              tocListContainer.scrollTop > firstVisibleLink.offsetTop)
+          ) {
+            // ...then scroll to center the link in the container
+            tocListContainer?.scrollTo({
+              top: Math.max(
+                0,
+                firstVisibleLink.offsetTop +
+                  firstVisibleLink.offsetHeight -
+                  tocListContainer.offsetHeight / 2
+              ),
+              behavior: "smooth",
+            });
+          }
         }
 
         if (!firstVisibleLink && previousSection) {
@@ -51,9 +85,9 @@ export const useHeadingIntersectionObserver = ({
         } else {
           link!.current!.classList.remove("toc-is-visible");
         }
-
-        highlightFirstActive();
       });
+
+      highlightFirstActive();
     };
 
     const observer = new IntersectionObserver(handleObserver, {
